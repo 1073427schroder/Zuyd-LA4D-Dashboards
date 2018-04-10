a5687d226e547ed3d9688fa8b6bdbd466d92cc18
1073427schroder <1073427schroder@zuyd.nl>
2018-04-10 09:27:20 +0200

    Removed test items, first version Study-Module done

--- Dashboard_App/src/app/study-module/study-module.component.ts
+++ Dashboard_App/src/app/study-module/study-module.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudyModule } from './study-module';
 import { MODULES } from './mock-study-modules';
-import { Test } from './test';
-import { TESTS } from './mock-test';
 
 
 @Component({
@@ -11,38 +9,7 @@ import { TESTS } from './mock-test';
   styleUrls: ['./study-module.component.css']
 })
 export class StudyModuleComponent implements OnInit {
-
-  test: Test = {
-    id: 0
-  };
-
-  tests: Test[] = TESTS;
-
-  selectedTest = this.test;
-  onSelect(n: number): void {
-    console.log("ID: " + JSON.stringify(n));
-    this.selectedTest = n;
-  }
-
-
-  /*
-  onSelect(n: number): void {
-    this.test = this.tests[n];
-  }
-  */
-
-  //sModule: StudyModule = {
-  //  id: 0,
-  //  name: 'NameModule1',
-  //  description: 'DescriptionModule1',
-  //  goal: 'GoalModule1',
-  //  learningOutcome: 'LearningOutcomeModule1',
-  //  semester: 1,
-  //  schoolyear: '2017/2018'
-  //};
-
   sModule: StudyModule;
-
   sModules: StudyModule[] = MODULES;
 
   onSelectM(moduleSelected: StudyModule): void {
@@ -52,7 +19,6 @@ export class StudyModuleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.test = this.tests[1];
   }
 
 }
