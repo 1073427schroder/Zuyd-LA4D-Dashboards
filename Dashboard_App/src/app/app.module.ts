b335ccf3131304f635272b44b77c95a370ef3186
1073427schroder <1073427schroder@zuyd.nl>
2018-05-09 10:15:54 +0200

    favicon added, table added to modulel

--- Dashboard_App/src/app/app.module.ts
+++ Dashboard_App/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { StudyModuleComponent } from './study-module/study-module.component';
 import { PrestatieIndicatorenComponent } from './prestatie-indicatoren/prestatie-indicatoren.component';
 import { DetailPrestatieIndicatorenComponent } from './detail-prestatie-indicatoren/detail-prestatie-indicatoren.component';
 import { environment } from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
@@ -18,14 +21,16 @@ import { environment } from '../environments/environment';
     AppComponent,
     StudyModuleComponent,
     PrestatieIndicatorenComponent,
-    DetailPrestatieIndicatorenComponent
+    DetailPrestatieIndicatorenComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
